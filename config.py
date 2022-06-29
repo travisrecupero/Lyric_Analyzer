b6107b3291fe76166cdcd6aedcd44c3f47ece84c
Travis Recupero <31936622+travisrecupero@users.noreply.github.com>
2022-06-29 14:51:03 -0400

    add config.py; will add back to gitignore after commit

---
+++ config.py
@@ -0,0 +1,4 @@
+genius_rapid_api_key = "" # https://rapidapi.com/Glavier/api/genius-song-lyrics1/
+genius_api_key = "" # https://genius.com/api-clients
+topic_tagging_api_key = "" # https://rapidapi.com/twinword/api/topic-tagging/
+
